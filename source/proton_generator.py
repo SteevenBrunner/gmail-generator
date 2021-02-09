791f66226fb9dfbe6663dba7d4044d7d8dda9f95
Steeven Brunner <steeven.brunner@epitech.eu>
2021-02-09 01:12:21 +0100

    proxy management

--- source/proton_generator.py
+++ source/proton_generator.py
@@ -6,11 +6,68 @@ import time
 import random
 import string
 
+import requests
+from proxyscrape import create_collector, get_collector
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+
+### Chrome needs to be open before starting the program ###
+
 
 # Printing funtion with 3 modes
 # 1 : Normal message
 # 2 : Information message
 # 3 : Caution message
+
+
+def proxy_connection():
+    collector = create_collector('my-collector', 'https')
+    proxy_status = "false"
+
+    while proxy_status == "false":
+
+        # Retrieve only 'us' proxies
+        proxygrab = collector.get_proxy({'code': ('fr')})
+        proxy = ("{}:{}".format(proxygrab.host, proxygrab.port))
+        print('\033[31m' + "Proxy:", proxy + '\033[0m')
+
+        try:
+            proxy_host = proxygrab.host
+            proxy_port = proxygrab.port
+            proxy_auth = ":"
+            proxies_file = {'http': 'http://{}@{}:{}/'.format(proxy_auth, proxy_host, proxy_port)}
+            requests.get("http://protonmail.com/", proxies=proxies_file, timeout=3.5)
+
+        except OSError:
+            print('\033[31m' + "Proxy Connection error!" + '\033[0m')
+            time.sleep(1)
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            proxy_status = "false"
+        else:
+            print('\033[31m' + "Proxy is working..." + '\033[0m')
+            time.sleep(1)
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            proxy_status = "true"
+            options = Options()
+            options.add_argument('--proxy-server={}'.format(proxy))
+
+    # Change Path to Chrome Driver Path (or move your ChromeDriver into the project folder)
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+
+    url = 'https://mail.protonmail.com/create/new?language=en'
+
+    driver.get(url)
+    driver.maximize_window()
+    driver.implicitly_wait(3)
 
 
 def msg(_option_, _message_):
@@ -30,132 +87,15 @@ def ext():
     sys.exit()
 
 
-# Function used to open Firefox
-def open_firefox():
-    # Printing basic message
-    msg(1, 'Opening Chrome...')
-
-    # Location the start button
-    _start_button_ = pyautogui.locateOnScreen('images/start_button.png')
-    _location_ = pyautogui.center(_start_button_)
-
-    # Clicking the start button
-    if not pyautogui.click(_location_):
-        msg(1, 'Opened start menu successfully!')
-    else:
-        msg(3, 'Failed to open start menu!')
-        ext()
-
-    time.sleep(2)
-
-    # Search for Firefox in the menu search
-    pyautogui.typewrite('chrome')
-    pyautogui.typewrite('\n')
-
-    # Print message
-    msg(1, 'Firefox is now open and running.')
-
-
-# Function used to locate GMail
-def locate_gmail():
-    # Sleep for a while and wait for Firefox to open
-    time.sleep(1)
-
-    # Printing message
-    msg(1, 'Opening Private navigation...')
-
-    _start_button_ = pyautogui.locateOnScreen('images/private_navigation.png')
-    _location_ = pyautogui.center(_start_button_)
-    # Clicking the start button
-    if not pyautogui.click(_location_):
-        msg(1, 'Opened start menu successfully!')
-    else:
-        msg(3, 'Failed to open start menu!')
-        ext()
-
-    _start_button_ = pyautogui.locateOnScreen('images/private_navigation_button.png')
-    _location_ = pyautogui.center(_start_button_)
-    # Clicking the start button
-    if not pyautogui.click(_location_):
-        msg(1, 'Opened start menu successfully!')
-    else:
-        msg(3, 'Failed to open start menu!')
-        ext()
-
-    # Printing message
-    msg(1, 'Opening Gmail...')
-
-    # Typing the website on the browser
-    pyautogui.keyDown('ctrlleft');
-    pyautogui.typewrite('a');
-    pyautogui.keyUp('ctrlleft')
-    pyautogui.typewrite('mail.protonmail.com/create/new?language=en')
-    pyautogui.typewrite('\n')
-
-    # Wait for a while until the website responds
-    time.sleep(1)
-
-    # Print a simple message
-    msg(1, 'Locating the form...')
-
-
-# Function used to randomize credentials
-def randomize(_option_, _length_):
-    if _length_ > 0:
-
-        # Options:
-        #       -p      for letters, numbers and symbols
-        #       -l      for letters only
-        #       -n      for numbers only
-        #       -m      for month selection
-        #       -d      for day selection
-        #       -y      for year selection
-
-        if _option_ == '-p':
-            string._characters_ = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+'
-        elif _option_ == '-l':
-            string._characters_ = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        elif _option_ == '-n':
-            string._characters_ = '1234567890'
-        elif _option_ == '-m':
-            string._characters_ = 'JFMASOND'
-
-        if _option_ == '-d':
-            _generated_info_ = random.randint(1, 28)
-        elif _option_ == '-y':
-            _generated_info_ = random.randint(1950, 2000)
-        else:
-            _generated_info_ = ''
-            for _counter_ in range(0, _length_):
-                _generated_info_ = _generated_info_ + random.choice(string._characters_)
-
-        return _generated_info_
-
-    else:
-        msg(3, 'No valid length specified...')
-        ext()
-
-
 # Function used to generate the credential information
-def generate_info(email_line):
-    time.sleep(3)
-
-    _start_button_ = pyautogui.locateOnScreen('images/choose_username_proton.png')
-    _location_ = pyautogui.center(_start_button_)
-    # Clicking the start button
-    if not pyautogui.click(_location_):
-        msg(1, 'Opened start menu successfully!')
-    else:
-        msg(3, 'Failed to open start menu!')
-        ext()
+def generate_info(username):
+    click_to_username()
 
     # Print message
     msg(1, 'Generating credentials...')
 
-
-
     # Username
-    _username_ = email_lines[email_counter]
+    _username_ = username
     pyautogui.typewrite(_username_)
     pyautogui.press('tab')
     pyautogui.press('tab')
@@ -182,27 +122,57 @@ def generate_info(email_line):
     pyautogui.press('enter')
 
 
+
+def click_to_username():
+    time.sleep(2)
+    _username_box_ = pyautogui.locateOnScreen('images/choose_username_proton.png')
+    _location_ = pyautogui.center(_username_box_)
+    # Clicking the start button
+    if not pyautogui.click(_location_):
+        msg(1, 'Clicked to username box successfully !')
+    else:
+        msg(3, 'Failed to click on username box !')
+        ext()
+
+
+def close_chrome():
+    _close_button_ = pyautogui.locateOnScreen('images/close_chrome_button.png')
+    _location_ = pyautogui.center(_close_button_)
+    # Clicking the start button
+    if not pyautogui.click(_location_):
+        msg(1, 'Chrome closed successfully !')
+    else:
+        msg(3, 'Failed to close Chrome !')
+        ext()
+
+
 # Main function
 if __name__ == '__main__':
+
+    # to increment email files
+    email_counter = 0
 
     # open email names file
     f = open('email_names.txt', 'r')
     email_lines = f.read().splitlines()
 
-    if open_firefox():
-        msg(3, 'Failed to execute "open_firefox" command.')
-        ext()
 
-    if locate_gmail():
-        msg(3, 'Failed to execute "locate_gmail" command.')
-        ext()
+    for i in range(1, 30):
+        for j in range(1, 3):
+            # connect to proxy, open ChromeDriver, launches ProtonMail signup page
+            proxy_connection()
 
-    # to increment email files
-    email_counter = 0
+            while email_counter < len(email_lines):
+                if generate_info(email_lines[email_counter]):
+                    msg(3, 'Failed to execute "generate_info" command.')
+                    ext()
 
-    if generate_info(email_lines[email_counter]):
-        msg(3, 'Failed to execute "generate_info" command.')
-        ext()
+                # close_chrome()
+
+                exit()
+                email_counter += 1
+
+
 
     msg(1, 'Done...')
     ext()
